b253abded4b540e3584d8494f3d53535c6f88361
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-19 20:34:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+end
+
+get '/name' do
+  "My name is #{name}"
+end
+
+get '/hometown' do
+  "My hometown is #{hometown}"
+end
+
+  get '/favorite-song'
+  "My favorite song is #{favorite-song}"
+end
+
 end
